59885d779591086b61381e59da19e56bbf0c8913
Gvinfinity <g.soares@ifpeopensource.com.br>
2024-09-08 06:35:41 -0300

    Fixes cannon minigame

--- livre-game-jam/scenes/cannon_minigame/scripts/cannon_player.gd
+++ livre-game-jam/scenes/cannon_minigame/scripts/cannon_player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
-@onready var marker_3d = $Pivot/Marker3D
+@onready var start_rotation_z = $"canhão/Cannon".rotation_degrees.z
+@onready var start_rotation_y = $"canhão/Cannon".rotation_degrees.y
 
 const CANNONBALL_SCENE = preload("res://scenes/cannon_minigame/cannonball.tscn")
 const MAX_ROTATION_Z = 10.0
@@ -8,40 +9,32 @@ const MIN_ROTATION_Z = -30.0
 const MAX_ROTATION_Y = 125.0
 const MIN_ROTATION_Y = 55.0
 
-# Variables to store the current rotation
-var current_rotation_z = 0.0
-var current_rotation_y = 0.0
+var CAN_SHOOT = true
 
-func _physics_process(delta: float):
+func _physics_process(_delta: float):
 	if Input.is_action_pressed("move_right"):
-		current_rotation_y -= 2
-		current_rotation_y = clamp(current_rotation_y, MIN_ROTATION_Y, MAX_ROTATION_Y)
-		rotation_degrees.y = current_rotation_y
+		rotation_degrees.y = clamp(rotation_degrees.y - 1.5, MIN_ROTATION_Y + start_rotation_y, MAX_ROTATION_Y + start_rotation_y)
 
 	if Input.is_action_pressed("move_left"):
-		current_rotation_y += 2
-		current_rotation_y = clamp(current_rotation_y, MIN_ROTATION_Y, MAX_ROTATION_Y)
-		rotation_degrees.y = current_rotation_y
+		rotation_degrees.y = clamp(rotation_degrees.y + 1.5,  MIN_ROTATION_Y + start_rotation_y, MAX_ROTATION_Y + start_rotation_y)
 
 	if Input.is_action_pressed("move_up"):
-		current_rotation_z += 1
-		current_rotation_z = clamp(current_rotation_z, MIN_ROTATION_Z, MAX_ROTATION_Z)
-		$"canhão/Cannon".rotation_degrees.z = current_rotation_z
+		$"canhão/Cannon".rotation_degrees.z = clamp($"canhão/Cannon".rotation_degrees.z + 1, MIN_ROTATION_Z + start_rotation_z, MAX_ROTATION_Z + start_rotation_y)
 
 	if Input.is_action_pressed("move_down"):
-		current_rotation_z -= 1
-		current_rotation_z = clamp(current_rotation_z, MIN_ROTATION_Z, MAX_ROTATION_Z)
-		$"canhão/Cannon".rotation_degrees.z = current_rotation_z
+		$"canhão/Cannon".rotation_degrees.z = clamp($"canhão/Cannon".rotation_degrees.z - 1, MIN_ROTATION_Z + start_rotation_z, MAX_ROTATION_Z + start_rotation_y)
 
-	if Input.is_action_just_pressed("shoot"):
-		var projeteis = get_tree().get_nodes_in_group("projeteis")
-		print(projeteis)
+	if Input.is_action_just_pressed("shoot") and CAN_SHOOT:
+		CAN_SHOOT = false
 		_shoot_cannon_ball()
+		await get_tree().create_timer(1.2).timeout
+		CAN_SHOOT = true
 
 func _shoot_cannon_ball():
 	var cannonball_node = CANNONBALL_SCENE.instantiate()
-	cannonball_node.position = Vector3(0, 0, 0)
-	cannonball_node.linear_velocity = ($"canhão/Cannon/Marker3D".position - cannonball_node.position).normalized() * 20
-	add_child(cannonball_node)
+	get_parent().add_child(cannonball_node)
+	cannonball_node.global_position = $"canhão/Cannon".global_position
+	print($"canhão/Cannon".rotation_degrees.z - start_rotation_z)
+	cannonball_node.linear_velocity = global_basis.x.rotated(global_basis.z, deg_to_rad($"canhão/Cannon".rotation_degrees.z - start_rotation_z)) * 22
 	
 	cannonball_node.add_to_group("projeteis")
