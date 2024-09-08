2b7793ddf5c2d585d01694230da9855a419b184b
Gvinfinity <g.soares@ifpeopensource.com.br>
2024-09-08 17:36:52 -0300

    Implements Barrel Game

--- livre-game-jam/scenes/minigames/minigame_barrel/scripts/barrel.gd
+++ livre-game-jam/scenes/minigames/minigame_barrel/scripts/barrel.gd
@@ -2,15 +2,20 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 
+var player_controlling = false;
+var points = 0;
 
 func _physics_process(delta: float) -> void:
+	if not player_controlling:
+		return;
+		
 	if not is_on_floor() and position.y > -4.5:
 		velocity += get_gravity() * delta 
 	if position.y < -4.5:
 		velocity -= get_gravity() * delta 
 
 	if Input.is_action_just_pressed("quit"):
-		$"../".exit_game(name.to_int()) 
+		$"/root/Main".exit_game(name.to_int()) 
 		get_tree().quit()
 	
 	var input_dir := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -24,7 +29,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print(body.name)
 	if body.is_in_group("Fruta"):
 		body.queue_free()
-		Global.points_barrel_minigame += 1
+		points += 1
