5d98cd2342acd6af44e15d4dbef3fb77d2f60634
André Yuhji Terada <andre.terada@ifood.com.br>
2024-09-08 12:00:48 -0300

    Player-pick

--- livre-game-jam/scenes/player/player.gd
+++ livre-game-jam/scenes/player/player.gd
@@ -4,47 +4,86 @@ extends CharacterBody3D
 const SPEED = 10.0
 const ANGULAR_ACELERATION = 7
 const JUMP_VELOCITY = 7
+
 @onready var cam = $SpringArm3D/Camera3D
 @onready var animation = $Body/PlayerRig/AnimationPlayer
+@onready var area_colision = $Body/Area3D/CollisionShape3D
+
+@export var current_animation = "idle"
+
+var is_capt = false
+var is_hold = false
+var holder:CharacterBody3D = null
+
+func captured(body: CharacterBody3D):
+	area_colision.disabled = true
+	is_capt = true
+	holder = body
+	
+func throw():
+	holder.linear_velocity = global_basis.x.rotated(global_basis.z, PI/4) * 22
+	holder.is_capt = false
+	holder = null
+	is_hold = false
+	
+	
+
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
 	
 func _ready():
 	cam.current = is_multiplayer_authority()
+	add_to_group("players")
 
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
-		# Add the gravity.
-		if not is_on_floor():
-			velocity += get_gravity() * 2 * delta
-
+		
 		if Input.is_action_just_pressed("quit"):
 			$"../".exit_game(name.to_int()) 
 			get_tree().quit()
 			
-		if Input.is_action_just_pressed("ui_accept"):
-			if is_on_floor():
-				velocity.y = JUMP_VELOCITY
-			animation.current_animation = "jump"
-			
-		if $Body/RayCastBottom.is_colliding() and not $Body/RayCastTop.is_colliding():
-			if $Body/RayCastMid.is_colliding():
-				velocity.y = JUMP_VELOCITY * 2.0/3
-			else:
-				velocity.y = JUMP_VELOCITY * 1.0/2
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var input_dir := Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
-								Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
-		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		if direction:
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
-			animation.current_animation = "run"
+		if is_capt:
+			position = holder.position + Vector3(0, 2, 0)
+			rotation = holder.rotation
+			current_animation = "jump"
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			velocity.z = move_toward(velocity.z, 0, SPEED)
-			animation.current_animation = "idle"
-		$Body.rotation.y = lerp_angle($Body.rotation.y, atan2(direction.x, direction.z), delta * ANGULAR_ACELERATION)
-		
+			# Add the gravity.
+			if not is_on_floor():
+				velocity += get_gravity() * 2 * delta
+				
+			if $Body/RayCastBottom.is_colliding() and not $Body/RayCastMid.is_colliding():
+				velocity.y = JUMP_VELOCITY * 2.0/3
+
+			# Get the input direction and handle the movement/deceleration.
+			# As good practice, you should replace UI actions with custom gameplay actions.
+			var input_dir := Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+									Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
+			var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+			if direction:
+				velocity.x = direction.x * SPEED
+				velocity.z = direction.z * SPEED
+				current_animation = "run"
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+				velocity.z = move_toward(velocity.z, 0, SPEED)
+				current_animation = "idle"
+			$Body.rotation.y = lerp_angle($Body.rotation.y, atan2(direction.x, direction.z), delta * ANGULAR_ACELERATION)
+			
+		if Input.is_action_just_pressed("ui_accept"):
+			if not is_hold:
+				if is_on_floor():
+					velocity.y = JUMP_VELOCITY
+				current_animation = "jump"
+			else:
+				throw()
+					
 		move_and_slide()
+		animation.current_animation = current_animation
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if is_hold == false and body.is_in_group("players") and body.name != self.name:
+		area_colision.disabled = true
+		body.captured(self)
+		is_hold = true
+		
