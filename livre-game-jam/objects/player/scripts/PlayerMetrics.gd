758dbd47edc561169f91e0e4f0ae9427f5265096
André Yuhji Terada <teradaandre@gmail.com>
2025-01-27 15:46:49 -0300

    Working-RB-player

---
+++ livre-game-jam/objects/player/scripts/PlayerMetrics.gd
@@ -0,0 +1,20 @@
+extends HBoxContainer
+
+@export var player: RigidBody3D
+
+@onready var acceleration = $Acceleration
+@onready var friction = $Friction
+@onready var speed = $Speed
+@onready var is_on_floor = $IsOnFloor
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	acceleration.text = "Acceleration: " + str(player.accel_multiplier).pad_decimals(2)
+	friction.text = "Friction: " + str(player.physics_material_override.friction).pad_decimals(2)
+	speed.text = "Speed: " + str(player.current_speed).pad_decimals(2)
+	is_on_floor.text = "Is on floor: " +  str(player.is_on_floor)
--- livre-game-jam/objects/player/scripts/rb_player.gd
+++ livre-game-jam/objects/player/scripts/rb_player.gd
@@ -1,12 +1,13 @@
 
 extends RigidBody3D
 
-@export var jump_velocity = 10
+@export var jump_velocity = 500
 @export var acceleration = 5
-@export var speed = 25
-@export var max_speed = 50
-@export_range(0.01,1.0) var stop_speed = 0.1
+@export var speed = 70
+@export var max_speed = 7
+@export_range(0.01,1.0) var stop_speed = 0.5
 @export var view_sensitivity = 10.0
+@export var current_speed = 0.0
 
 var accel_multiplier = 1.0
 var velocity=Vector3()
@@ -15,7 +16,8 @@ var mouse_input = Vector2()
 @onready var head = $CameraPivot
 @onready var eyes = $CameraPivot/SpringArm3D/Camera3D
 @onready var body = $Body
-@onready var feet = $Feet
+@onready var feet = %Feet
+@onready var feet_detector = %Feet/RayCast3D
 
 var is_on_floor = false
 var move_input = Vector2.ZERO
@@ -33,12 +35,14 @@ func _physics_process(delta):
 	move_input = Vector2.ZERO
 	var dir = Vector3()
 	#movement input
-	move_input = Input.get_vector("move_left","move_right","move_down","move_up")
-	dir += move_input.x*head.global_transform.basis.x;
-	dir -= move_input.y*head.global_transform.basis.z;
-	velocity = lerp(velocity,dir*speed,acceleration*accel_multiplier*delta)
-	add_constant_central_force(velocity)
-	if feet.is_colliding():
+	move_input = Input.get_vector("move_left","move_right","move_up", "move_down")
+	dir = (transform.basis * Vector3(move_input.x, 0, move_input.y)).normalized()
+	var dir_rotated = dir.rotated(Vector3(1, 0, 0), feet.rotation.x).rotated(Vector3(0, 0, 1), feet.rotation.z)
+	velocity = dir_rotated * speed
+
+	
+	if feet_detector.is_colliding():
+		apply_central_impulse(velocity)
 		is_on_floor = true
 		physics_material_override.friction = 1.0
 		accel_multiplier = 1.0
@@ -46,17 +50,13 @@ func _physics_process(delta):
 		accel_multiplier = 0.1
 		is_on_floor = false
 		apply_central_impulse(Vector3.UP * jump_velocity)
-	#view and rotation
-	eyes.rotation_degrees.x -= mouse_input.y * view_sensitivity * delta;
-	eyes.rotation_degrees.x = clamp(eyes.rotation_degrees.x,-80,80)
-	head.rotation_degrees.y -= mouse_input.x * view_sensitivity * delta;
 	mouse_input =Vector2.ZERO
 	
 
 func _integrate_forces(state):
 	#limit max speed
-	if state.linear_velocity.length()>max_speed:
-		state.linear_velocity=state.linear_velocity.normalized()*max_speed
+	if state.linear_velocity.length() > max_speed:
+		state.linear_velocity = state.linear_velocity.normalized()*max_speed
 	#artificial stopping movement i.e not using physics
 	if move_input.length() < 0.2:
 		state.linear_velocity.x = lerp(state.linear_velocity.x,0.0,stop_speed)
@@ -64,7 +64,9 @@ func _integrate_forces(state):
 	#push against floor to avoid sliding on "unreasonable" slopes
 	if state.get_contact_count() > 0 and move_input.length()< 0.2:
 		if is_on_floor and state.get_contact_local_normal(0).y < 0.9:
-			add_constant_central_force(-state.get_contact_local_normal(0)*10)
+			apply_central_force(-state.get_contact_local_normal(0)*10)
+			
+	current_speed = state.linear_velocity.length()
 
 #mouse input
 func _input(event):
